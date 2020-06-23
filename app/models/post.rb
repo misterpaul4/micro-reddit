de08aae0097b8f9805200878a739df8367da0558
Cathella <ncathie214@gmail.com>
2020-06-24 00:13:30 +0300

    Add validations and associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comment
+  has_many :comments
+
+  validates(:title, presence: true, length: { minimum: 10, maximum: 30 })
+  validates(:body, presence: true, length: { maximum: 100 })
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-  has_many :post
+  has_many :posts
+  has_many :comments
+
+  validates(:username, :password, presence: true, length: { minimum: 5, maximum: 20 })
 end
